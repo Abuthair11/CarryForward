23aef62368e93bc6b982ed8590ba20efa2826662
Abuthair <abuthairsheika11@gmail.com>
2023-09-22 16:42:44 +0100

    AG Sequence Problem

--- src/Special_SubsequencesAG.java
+++ src/Special_SubsequencesAG.java
@@ -1,10 +1,25 @@
+//You have given a string A having Uppercase English letters.
+//
+//You have to find how many times subsequence "AG" is there in the given string.
+//
+//NOTE: Return the answer modulo 109 + 7 as the answer can be very large.
 
-package PACKAGE_NAME;/*
-	 *Created by Abuthair
-	 * Date:22/09/2023
-	 * time:16:36
-	 *projectName:CarryForward
-	 
-	 */
+ //A = "ABCGAG"
+//output =3
 public class Special_SubsequencesAG {
-}
+     public int solve(String A) {
+
+         int c = 0;
+         int ans = 0;
+         for (int i = A.length() - 1; i >= 0; i--) {
+             if (A.charAt(i) == 'G') {
+                 c++;
+             }
+             if (A.charAt(i) == 'A') {
+                 ans += c;
+             }
+
+         }
+         return ans;
+     }
+ }
